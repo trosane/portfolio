7afd854272d4436bef51410f19e9510973280346
Trent Rosane <trosane@uw.edu>
2015-11-22 13:17:48 -0800

    SPA trouble

--- js/app.js
+++ js/app.js
@@ -1,31 +1,26 @@
 $(document).foundation();
 
 // Create app
-angular.module('myApp', ['ui.router'])
-	.controller("myCtrl", function($scope, $http) {
-		$http.get('data.json').success(function (data) {
-			$scope.data = data;
-		})
+var app = angular.module('myApp', ['ui.router']);
+	.controller("myCtrl", function($scope) {
+
 	})
 
 // Configure app
-.config(function($stateProvider) {
-  $stateProvider.state('home', { // Landing page
-  url:'/index.html',
-  templateUrl: 'index.html', // HTML fragment
-  controller: 'HomeController', // Which controller 
-  })
+app.config(function($stateProvider, $urlRouterProvider) {
+  
+  $urlRouterProvider.otherwise('/home');
 
-  .state('about', { // Landing page
-  url:'/about',
-  templateUrl: 'about.html', // HTML fragment
-  controller: 'AboutController', // Which controller 
+  $stateProvider.state('home', { // Landing page
+	  url:'/home',
+	  templateUrl: 'home.html', // HTML fragment
+	  controller: 'HomeController', // Which controller 
   })
 
   .state('projects', { // Landing page
-  url:'/projects',
-  templateUrl: 'projects.html', // HTML fragment
-  controller: 'ProjectsController', // Which controller 
+	  url:'/projects',
+	  templateUrl: 'projects.html', // HTML fragment
+	  controller: 'ProjectsController', // Which controller 
   })
 })
 
@@ -33,22 +28,12 @@ angular.module('myApp', ['ui.router'])
 .controller('HomeController', function($scope, $http){
 	$http.get('data.json').success(function (data) {
 		$scope.data = data;
-		console.log($scope.data);
 	});
-
-	console.log($scope.data);
 })
-
-
-// About page controller: define $scope.about as a string
-.controller('AboutController', function($scope){
-  $scope.stuff = "Some info"
-  $scope.about = "Hello"
-})
-
 
 // Content controller: define $scope.url as an image
-.controller('ProjectsController', function($scope){
-  $scope.stuff = "Projects"
-  $scope.stuff = "blah"
+.controller('ProjectsController', function($scope, $http){
+  $http.get('data.json').success(function (data) {
+		$scope.data = data;
+	});
 })
